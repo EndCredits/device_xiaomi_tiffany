c9a031911fee7f3eddce47faf61586467b52b9dc
EndCredits <alicization.han@gmail.com>
2021-11-06 03:10:10 +0800

    tiffany: ArrowOS ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tiffany.mk
+    $(LOCAL_DIR)/arrow_tiffany.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosp_tiffany-user \
-	aosp_tiffany-userdebug \
-	aosp_tiffany-eng
+	arrow_tiffany-user \
+	arrow_tiffany-userdebug \
+	arrow_tiffany-eng
