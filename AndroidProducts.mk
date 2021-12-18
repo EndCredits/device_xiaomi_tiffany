57d30ca2f6e2b4c05cb2066dd3211f1c0925d0e0
EndCredits <alicization.han@gmail.com>
2021-12-18 19:11:15 +0800

    tiffany: DerpFest ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tiffany.mk
+    $(LOCAL_DIR)/derp_tiffany.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosp_tiffany-user \
-	aosp_tiffany-userdebug \
-	aosp_tiffany-eng
+	derp_tiffany-user \
+	derp_tiffany-userdebug \
+	derp_tiffany-eng
--- derp_tiffany.mk
+++ derp_tiffany.mk
@@ -22,12 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tiffany/device.mk)
 
 # Inherit some common sakura stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tiffany
-PRODUCT_NAME := aosp_tiffany
+PRODUCT_NAME := derp_tiffany
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 5X
@@ -36,11 +36,10 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# PixelExperience Specific Settings
-# TARGET_GAPPS_ARCH := arm64
+# DerpFest Specific Settings
 TARGET_BOOT_ANIMATION_RES := 1080
-# TARGET_INCLUDE_STOCK_ARCORE := false
-# TARGET_INCLUDE_LIVE_WALLPAPERS := false
+DERP_BUILDTYPE := Official
+WITH_GMS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tiffany-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
